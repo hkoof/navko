e188cd5cd6a7e273740175ab845a5b7a05e99aed
Heiko Noordhof <h.e.noordhof@rug.nl>
2024-05-27 22:07:02 +0200

    e6b(), but not correct

--- pos.py
+++ pos.py
@@ -14,28 +14,33 @@ class Point(geopy.point.Point):
 
 class Leg:
     def __init__(self, begin, bearing, distance, checkpoints=[]):
-        self._begin = begin
-        self._bearing = bearing
-        self._distance = geodesic(nautical=distance)
-        self._checkpoints = checkpoints
-
-    def begin(self):
-        return #self._begin
-
-    def distance(self):
-        return self._distance
-
-    def bearing(self):
-        return #self._bearing
+        self.begin = begin
+        self.bearing = bearing
+        self.distance = geodesic(nautical=distance)
+        self.checkpoints = checkpoints
 
     def end(self):
-        endpos = self._distance.destination(self._begin, bearing=self._bearing)
+        endpos = self.distance.destination(self.begin, bearing=self.bearing)
         return endpos
-        
+
+    def e6b(self, true_airspeed, wind_direction, wind_speed):
+        # Thanks to https://aviation.stackexchange.com/questions/46741/
+        #
+        tt = math.radians(self.bearing)  
+        th = tt + math.asin(
+                (wind_speed / true_airspeed) *
+                math.sin(math.radians(wind_direction - self.bearing))
+            )
+
+        gs = (true_airspeed * math.sin(th) - wind_speed * math.sin(wind_direction)) \
+                / math.sin(tt)
+
+        return ( math.degrees(th) % 360, gs, )
+
     def geojson(self):
         end = self.end()
         lines = gj.LineString([
-            (self._begin.longitude, self._begin.latitude),
+            (self.begin.longitude, self.begin.latitude),
             (end.longitude, end.latitude),
         ])
         feature = gj.Feature(geometry=lines)
@@ -45,7 +50,10 @@ class Leg:
 def main():
     begin = (53.088, 6.0804,)
     leg1 = Leg(Point(begin), 212.0, 38.0)
-    print(leg1.geojson())
+    th, gs = leg1.e6b(100, 200, 20)
+    print("th =", th)
+    print("gs =", gs)
+    #print(leg1.geojson())
 
 if __name__ == "__main__":
     main()
