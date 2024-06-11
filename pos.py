d3f5a36ecd2ffa97f9843f67ecc06114a33b9bc3
Heiko Noordhof <h.e.noordhof@rug.nl>
2024-06-11 22:21:00 +0200

    scratch #1 nothing works, but defining yaml route formats. New plan: route is a specialized (inherits) list of legs

--- pos.py
+++ pos.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import math
+
 import geojson as gj
 
 from geopy.distance import geodesic
@@ -8,24 +9,29 @@ import geopy.point
 
 
 class Point(geopy.point.Point):
-    def __init__(self, latitude=None, longitude=None, altitude=None, label=''):
+    def __init__(self, latitude=None, longitude=None, label=''):
         self.label = label
-        super().__init__()
+        super().__init__(altitude=None)
 
-class Leg:
-    def __init__(self, begin, bearing, distance, checkpoints=[]):
-        self.begin = begin
+class Leg(list):
+    def __init__(self, startpoint, bearing, distance, checkpoints=[]):
+        # Leg: list of Points.
+        #
+        # The points parameter should be a sequence of tuples:
+        #  ( <label>, <distance from begin> )
+        # Internally these are converted to Points. 
+        #
         self.bearing = bearing
         self.distance = geodesic(nautical=distance)
-        self.checkpoints = checkpoints
+        end = self.distance.destination(self.begin, bearing=self.bearing)
+        # FIXME: validate begin
+        self._points = list(begin)
+        for label, dist in checkpoints:
 
-    def end(self):
-        endpos = self.distance.destination(self.begin, bearing=self.bearing)
-        return endpos
+
+
 
     def e6b(self, true_airspeed, wind_direction, wind_speed):
-        # Thanks: https://www.omnicalculator.com/physics/ground-speed
-        #
         tt = math.radians(self.bearing)  
         wd = math.radians(wind_direction + 180)
 
@@ -33,7 +39,7 @@ class Leg:
         wind_correction_angle = math.degrees(wca)
         
         ground_speed = math.sqrt(
-                math.pow(true_airspeed, 2) + math.pow(wind_speed, 2) -
+                true_airspeed**2 + wind_speed**2 -
                 (
                     2 * true_airspeed * wind_speed * math.cos(tt - wd + wca)
                 )
@@ -47,17 +53,20 @@ class Leg:
             (self.begin.longitude, self.begin.latitude),
             (end.longitude, end.latitude),
         ])
-        feature = gj.Feature(geometry=lines)
-        feature_collection = gj.FeatureCollection( [feature] )
-        return gj.dumps(feature_collection, sort_keys=True)
+        feature1 = gj.Feature(geometry=lines)
+        feature2 = gj.Feature(geometry=gj.MultiPoint(self.checkpoints),
+                              properties = {'name': 'QrQ', 'number': 7}
+                              )
+        feature_collection = gj.FeatureCollection( [feature1, feature2] )
+        return gj.dumps(feature_collection) #, sort_keys=True)
 
 def main():
     begin = (53.088, 6.0804,)
-    leg1 = Leg(Point(begin), 212.0, 38.0)
+    leg1 = Leg(Point(begin), 212.0, 38.0, [ (6.08,53.08), (6.09,53.09,) ] )
     th, gs = leg1.e6b(100, 212, 20)
-    print("th =", th)
-    print("gs =", gs)
-    #print(leg1.geojson())
+    #print("th =", th)
+    #print("gs =", gs)
+    print(leg1.geojson())
 
 if __name__ == "__main__":
     main()
---
+++ testlist.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+class StringList(list):
+    def __init__(self, iterable):
+        super().__init__(str(item) for item in iterable)
+
+    def __setitem__(self, index, item):
+        super().__setitem__(index, str(item))
+
+    def insert(self, index, item):
+        super().insert(index, str(item))
+
+    def append(self, item):
+        super().append(str(item))
+
+    def extend(self, other):
+        if isinstance(other, type(self)):
+            super().extend(other)
+        else:
+            super().extend(str(item) for item in other)
+
+l = list(('app', 10,2,3,46))
+lst = l[2:4]
+print(lst)
+for i in lst:
+    print("==>", i)
