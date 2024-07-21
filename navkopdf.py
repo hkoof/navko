dea80e26bf772120c37ff334fd941faa67cc3eb7
Heiko Noordhof <h.e.noordhof@rug.nl>
2024-07-21 17:41:17 +0200

    WIP: pdf navlog

--- navkopdf.py
+++ navkopdf.py
@@ -1,19 +1,24 @@
 from fpdf import FPDF
+from fpdf.fonts import FontFace
+from fpdf.drawing import DeviceGray, DeviceRGB
 
 def navlog2pdf(navlog, pdf_path):
     pdf = FPDF(orientation="landscape", format="A4")
     pdf.add_page()
     pdf.set_font("Courier", size=12)
 
-    with pdf.table() as table:
+    chkp_colspan = 5
+    style = FontFace(color=DeviceGray(0), fill_color=DeviceGray(0.93)) 
+
+    with pdf.table(line_height=12) as table:
         header = table.row()
         startrow = table.row()
 
         header.cell('Leg') ; startrow.cell('0', align='R')
         header.cell('Acc') ; startrow.cell('0', align='R')
-        header.cell('ETO') ; startrow.cell(' ')
-        header.cell('ATO') ; startrow.cell(' ')
-        header.cell('Checkpoint', colspan=4) ; startrow.cell(navlog.start_name, colspan=4)
+        header.cell('ETO', colspan=2) ; startrow.cell('_', align='C', colspan=2, padding=(0,0,2,0,), style=style )
+        header.cell('ATO', colspan=2) ; startrow.cell(' ', colspan=2, style=style)
+        header.cell('Checkpoint', colspan=chkp_colspan) ; startrow.cell(navlog.start_name, colspan=chkp_colspan)
         header.cell('Alt') ; startrow.cell('')
         header.cell('MH') ; startrow.cell('')
         header.cell('TH') ; startrow.cell('')
@@ -29,22 +34,22 @@ def navlog2pdf(navlog, pdf_path):
 
             row.cell(f'{leg.time}', align='R')
             row.cell(f'{leg.time_acc}', align='R')
-            row.cell('')
-            row.cell('')
-            row.cell(f'{leg.name}', colspan=4)
+            row.cell('', colspan=2, style=style)
+            row.cell('', colspan=2, style=style)
+            row.cell(f'{leg.name}', colspan=chkp_colspan)
 
-            row.cell(f'{leg.alt:>}' if leg.alt else '', align='R')
-            row.cell(f'{leg.mh:0>3d}' if leg.mh else '', align='R')
-            row.cell(f'{leg.th:0>3d}' if leg.th else '', align='R')
+            row.cell(f'{leg.alt:>}' if leg.alt != None else '', align='R')
+            row.cell(f'{leg.mh:0>3d}' if leg.mh != None else '', align='R')
+            row.cell(f'{leg.th:0>3d}' if leg.th != None else '', align='R')
 
             wca_str = ''
             if leg.wca == 0: wca_str = '0'
             elif leg.wca != None: wca_str = f'{leg.wca:>+d}'
             row.cell(wca_str, align='R')
 
-            row.cell(f'{leg.tt:0>3d}' if leg.tt else '', align='R')
-            row.cell(f'{leg.tas:>}' if leg.tas else '', align='R')
-            row.cell(f'{leg.gs:>}' if leg.gs else '', align='R')
+            row.cell(f'{leg.tt:0>3d}' if leg.tt != None else '', align='R')
+            row.cell(f'{leg.tas:>}' if leg.tas != None else '', align='R')
+            row.cell(f'{leg.gs:>}' if leg.gs != None else '', align='R')
 
             row.cell(f'{leg.dist:>4.0f}', align='R')
             row.cell(f'{leg.dist_acc:>4.0f}', align='R')
