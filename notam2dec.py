037e26d24088f4324ad49eb47b994dc967e0a1e5
Heiko Noordhof <h.e.noordhof@rug.nl>
2024-11-01 17:28:02 +0100

    notam2dec refactor: old functionality OK now

--- notam2dec.py
+++ notam2dec.py
@@ -3,46 +3,62 @@
 import sys
 import re
 
+import argparse
+
+pattern_lat = '([0-9]{2})([0-9]{2})([0-9]{2}){0,1}([NS])'
+pattern_lon = '([0-9][0-9]{2})([0-9]{2})([0-9]{2}){0,1}([EW])'
+pattern_coord = pattern_lat + ' ' + pattern_lon
+
+re_coord = re.compile(pattern_coord)
+
+def notam2dec(notam_coordinates):
+
+    print("DBG: coord: ", notam_coordinates)
+
+    m_coord = re.match(re_coord, notam_coordinates)
+    if m_coord == None:
+        print(f'notam format error: {notam_coordinates}')
+        sys.exit(2)
+
+    grp_coord = m_coord.groups()
+    lat_dec = (1 if grp_coord[3] == 'N' else -1) * (
+                  int(grp_coord[0]) +
+                  int(grp_coord[1]) / 60.0 +
+                  int(grp_coord[2]) / 3600.0
+                )
+    lon_dec = (1 if grp_coord[7] == 'E' else -1) * (
+                  int(grp_coord[4]) +
+                  int(grp_coord[5]) / 60.0 +
+                  int(grp_coord[6]) / 3600.0
+                )
+    return f'{lat_dec:.5f} {lon_dec:.5f}'
+
+
 def main():
-    nargs = len(sys.argv)
-    if nargs == 3:
-        lat_notam = sys.argv[1]
-        lon_notam = sys.argv[2]
-    else:
-        print(f'Usage: {sys.argv[0]} <coordinates in notam format>', file=sys.stderr)
-        sys.exit(1)
+    parser = argparse.ArgumentParser(
+        prog='notam_coordinates',
+        description='Convert notam coordinates',
+    )
+    parser.add_argument('-c', '--coordinates', nargs=2)
+    parser.add_argument('-f', '--file')
+    parser.add_argument(
+        '--geojson',
+        help='Write to geojson file',
+    )
+    args = parser.parse_args()
 
-    #print("lat: ", lat_notam)
-    #print("lon: ", lon_notam)
+    if args.coordinates:
+        dec_coords = notam2dec(args.coordinates[0] + ' ' + args.coordinates[1])
+        print(dec_coords)
+        sys.exit(0)
 
-    re_lat = re.compile('([0-9]{2})([0-9]{2})([0-9]{2}){0,1}([NS])')
-    re_lon = re.compile('([0-9][0-9]{2})([0-9]{2})([0-9]{2}){0,1}([EW])')
+    if args.geojson:
+        with open(args.geojson, 'w') as output:
+            for route in routes:
+                print(route.geojson(), file=output)
 
-    m_lat = re.match(re_lat, lat_notam)
-    if m_lat == None:
-        print(f'notam format error in latitude part: {lat_notam}')
-        sys.exit(2)
+    notam_coordinates = list()
 
-    m_lon = re.match(re_lon, lon_notam)
-    if m_lon == None:
-        print(f'notam format error in longitude part: {lon_notam}')
-        sys.exit(2)
-
-    grp_lat = m_lat.groups()
-    lat_dec = (1 if grp_lat[3] == 'N' else -1) * (
-                  int(grp_lat[0]) +
-                  int(grp_lat[1]) / 60.0 +
-                  int(grp_lat[2]) / 3600.0
-                )
-
-    grp_lon = m_lon.groups()
-    lon_dec = (1 if grp_lon[3] == 'E' else -1) * (
-                  int(grp_lon[0]) +
-                  int(grp_lon[1]) / 60.0 +
-                  int(grp_lon[2]) / 3600.0
-                )
-
-    print(f'{lat_dec:.5f} {lon_dec:.5f}')
 
 if __name__ == "__main__":
     main()
